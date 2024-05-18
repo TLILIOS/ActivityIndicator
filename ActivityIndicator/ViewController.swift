358e2d3087b83d3d3cd904d23cae244db9c8f8d7
TLiLi HAMDI <tlilios@yahoo.com>
2024-05-18 18:47:30 +0200

    Starting

---
+++ ActivityIndicator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActivityIndicator
+//
+//  Created by HTLILI on 18/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
